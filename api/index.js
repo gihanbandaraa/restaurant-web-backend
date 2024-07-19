8d965b50fb03e2253b62043d90435b180a407a4a
Gihan Bandara <63912749+gihanbandaraa@users.noreply.github.com>
2024-07-19 22:20:29 +0530

    create test API route

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-// Connect to MongoDB
+// Import routes
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING).then(() => {
@@ -15,3 +16,4 @@ app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
 
+app.use("/api/user", userRoutes);
