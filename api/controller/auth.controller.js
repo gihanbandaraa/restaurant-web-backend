8e0edb7682f6afb3970a29fc683549c42a14c3b3
Gihan Bandara <63912749+gihanbandaraa@users.noreply.github.com>
2024-07-21 15:01:38 +0530

    create signin route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -31,3 +31,34 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      next(errorHandler(404, "User not found"));
+    }
+
+    const isPasswordValid = bcryptjs.compareSync(password, user.password);
+    if (!isPasswordValid) {
+      next(errorHandler(400, "Invalid password"));
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "12h",
+    });
+
+    const { password: userPassword, ...userWithoutPassword } = user._doc;
+    return res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(userWithoutPassword);
+  } catch (error) {
+    return next(error);
+  }
+};
