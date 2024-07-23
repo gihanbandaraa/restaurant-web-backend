786b5b56e6a597843e01b398958afddf775571aa
Gihan Bandara <63912749+gihanbandaraa@users.noreply.github.com>
2024-07-23 10:22:29 +0530

    add google authentication route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -62,3 +62,48 @@ export const signin = async (req, res, next) => {
     return next(error);
   }
 };
+
+export const google = async (req, res, next) => {
+  const { name, email, googlePhotoUrl } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "12h",
+      });
+      const { password: userPassword, ...userWithoutPassword } = user_doc;
+      return res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(userWithoutPassword);
+    } else {
+      const generatedPassword = Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const newUser = new User({
+        username:
+          name.toLowerCase().split(" ").join("") +
+          Math.random().toString(9).slice(-4),
+        email,
+        password: hashedPassword,
+        profilePicture: googlePhotoUrl,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: "12h",
+      });
+      const { password: userPassword, ...userWithoutPassword } = newUser._doc;
+      return res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(userWithoutPassword);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
