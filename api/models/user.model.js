a5fd95de6805f4ebdbedf56bd4d4a33d31a39244
Gihan Bandara <63912749+gihanbandaraa@users.noreply.github.com>
2024-08-06 09:57:57 +0530

    add staff auth as well

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -25,6 +25,10 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    isStaff: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true }
 );
