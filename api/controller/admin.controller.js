4b16a42aa6b6de01500ba60446ed0d6ad9db7781
Gihan Bandara <63912749+gihanbandaraa@users.noreply.github.com>
2024-08-06 19:27:43 +0530

    finish the crud operations in category

--- api/controller/admin.controller.js
+++ api/controller/admin.controller.js
@@ -4,7 +4,61 @@ export const addCategory = async (req, res, next) => {
   const { name } = req.body;
   try {
     const category = await Category.create({ name });
-    return res.status(201).json(category);
+    return res.status(201).json({ success: true, category });
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.status(400).json({
+        success: false,
+        message: "Category name already exists!",
+      });
+    }
+    next(error);
+  }
+};
+
+export const updateCategory = async (req, res, next) => {
+  const { name } = req.body;
+  const { id } = req.params;
+
+  try {
+    const category = await Category.findByIdAndUpdate(
+      id,
+      { name },
+      { new: true }
+    );
+
+    if (!category) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Category not found" });
+    }
+    return res.status(200).json({ success: true, category });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteCategory = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const category = await Category.findByIdAndDelete(id);
+
+    if (!category) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Category not found" });
+    }
+    return res.status(200).json({ success: true, message: "Category deleted" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getCategories = async (req, res, next) => {
+  try {
+    const categories = await Category.find();
+    return res.status(200).json(categories);
   } catch (error) {
     next(error);
   }
--- api/routes/admin.route.js
+++ api/routes/admin.route.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { addCategory } from "../controller/admin.controller.js";
+import { addCategory, getCategories,updateCategory,deleteCategory } from "../controller/admin.controller.js";
 
 const router = express.Router();
 
 router.post("/add-category", addCategory);
+router.get("/get-categories", getCategories);
+router.put("/update-category/:id", updateCategory);
+router.delete("/delete-category/:id", deleteCategory);
 
 export default router;
